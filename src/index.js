7c9d24c73f9771d68bc1d44471832f4ba5cb6c1b
pyeong.oh5 <pyeong.oh5@gmail.com>
2020-05-24 02:02:05 +0900

    chore: code 정리

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// require("babel-core/register");
-// require("babel-polyfill");
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
@@ -19,7 +17,6 @@ const overviewLinks = [];
 const filename = `${new Date().toISOString().split('T')[0]}-${Date.now()}.txt`;
 let processCount;
 if (process.argv) {
-  console.log('process.argv', process.argv);
   urls = process.argv[2] && process.argv[2].trim().split('url=')[1].split(',');
   processCount = process.argv[3] && process.argv[3].trim().split('=')[1];
 }
@@ -34,29 +31,22 @@ if (!urls) {
 const { ServiceBuilder } = require('selenium-webdriver/chrome');
 const { Builder } = require('selenium-webdriver');
 
-// const chromeDriverPath = path.resolve("chromedriver_83");
+// 현재는 chrome 83버전을 기준으로만 지원함
 const driverName = `chromedriver_83${isMacOS ? '' : '_win.exe'}`;
 const chromeDriverPath = path.resolve(driverName);
-console.log('chromeDriverPath', chromeDriverPath);
 const serviceBuilder = new ServiceBuilder(chromeDriverPath);
 
-console.log('serviceBuilder', serviceBuilder);
 const driver = new Builder()
   .forBrowser('chrome')
   .setChromeService(serviceBuilder)
-  // .setChromeOptions(chromeOptions)
   .build();
-// const url = 'https://itp.ne.jp/genre/?area=13&genre=3&subgenre=70&sort=01&sbmap=false';
-// urls = ['https://itp.ne.jp/genre/?area=13&genre=3&subgenre=70&sort=01&sbmap=false'];
 
 const start = Date.now();
 
 const startCrawling = async (url) => {
-  console.log('startCrawling', overviewLinks.length);
   await driver.get(url);
   await spreadAllList();
   const titleList = await (await driver).findElements(By.className('m-article-card__header__title__link'));
-  console.log('done', titleList.length);
   
   for (let i = 0; i < titleList.length; ++i) {
     const titleElement = titleList[i];
@@ -98,15 +88,12 @@ const parsingEmail = async(_driver) => {
     let link = overviewLinks.shift();
     let findMail = false;
     try {
-      console.log('link: ', link);
       await _driver.get(link);
-      // #__layout > div > article > div > div > div > main > div > dl:nth-child(7) > dd > a:nth-child(1)
       const el = await _driver.findElement(By.css('a[href*="mailto"]'));
-      // const text = await el.getText();
-      console.log('el', el);
       const text = await el.getAttribute('href');
-      emails.push(text.split('mailto:')[1]);
-      console.log('email: ', text);
+      const email = text.split('mailto:')[1];
+      console.log('find email:', email);
+      emails.push(email);
       findMail = true;
     } catch(e) {
       console.log('email is not exist in', link);
@@ -114,14 +101,12 @@ const parsingEmail = async(_driver) => {
     if (!findMail) {
       try {
         link += 'shop'
-        console.log('link: ', link);
         await _driver.get(link);
-        // .item-body.basic dl:nth-child(10) dd a 
         const el = await _driver.findElement(By.css('a[href*="mailto"]'));
-        console.log('el', el);
         const text = await el.getAttribute('href');
-        emails.push(text.split('mailto:')[1]);
-        console.log('email: ', text);
+        const email = text.split('mailto:')[1];
+        console.log('find email:', email);
+        emails.push(email);
       } catch(e) {
         console.log('email is not exist in', link);
       }
@@ -152,98 +137,23 @@ const saveToFile = () => {
   });
 }
 
-// const getUrl = async (url) => {
-//   await driver.get(url);
-//   await spreadAllList();
-//   const titleList = await (await driver).findElements(By.className('m-article-card__header__title__link'));
-//   console.log('done', titleList.length);
-  
-//   for (let i = 0; i < titleList.length; ++i) {
-//     const titleElement = titleList[i];
-//     const href = await titleElement.getAttribute('href');
-//     overviewLinks.push(href);
-//   }
-//   // console.log('overviewLinks', overviewLinks.length, os.cpus());
-  
-//   for(let i = 0; i < overviewLinks.length; ++i) {
-//     let link = overviewLinks[i];
-//     let findMail = false;
-//     try {
-//       console.log('link: ', link);
-//       await driver.get(link);
-//       // #__layout > div > article > div > div > div > main > div > dl:nth-child(7) > dd > a:nth-child(1)
-//       const el = await driver.findElement(By.css('a[href*="mailto"]'));
-//       // const text = await el.getText();
-//       console.log('el', el);
-//       const text = await el.getAttribute('href');
-//       emails.push(text);
-//       console.log('email: ', text);
-//       findMail = true;
-//     } catch(e) {
-//       console.log('email is not exist in', link);
-//     }
-//     if (!findMail) {
-//       try {
-//         link += 'shop'
-//         console.log('link: ', link);
-//         await driver.get(link);
-//         // .item-body.basic dl:nth-child(10) dd a 
-//         const el = await driver.findElement(By.css('a[href*="mailto"]'));
-//         console.log('el', el);
-//         const text = await el.getAttribute('href');
-//         emails.push(text);
-//         console.log('email: ', text);
-//       } catch(e) {
-//         console.log('email is not exist in', link);
-//       }
-//     }
-//   }
-//   console.log('emails', emails.length);
-//   // fs.writeFile(path.join(os.homedir(), 'mail-list.txt'), emails.join('\n'), (err) => {
-//   if (urls.length > 0) {
-//     const url = urls.shift();
-//     await getUrl(url);
-//   } else {
-//     fs.writeFile(path.resolve(filename), emails.join('\r\n'), (err) => {
-//       if (err) {
-//         console.log('writing error', err);
-//         return;
-//       }
-//       const end = Date.now();
-//       console.log('write done!', end - start, 'ms');
-//       driver.quit();
-//     });
-//   }
-// };
-
+/**
+ * 더보기 버튼을 통해 pagination이 적용된 페이지 이므로, 해당 페이지에서 제공하는 모든 리스트를 가져오기 위해서
+ * 더보기 버튼이 존재할 때까지 클릭을 시도함
+ */
 const spreadAllList = async () => {
   return new Promise(async (resolve, reject) => {
     try {
       while(true) {
         const moreButton = await driver.findElement(By.className('m-read-more'));
-        // console.log('moreButton', moreButton);
         if (moreButton) {
           await moreButton.click();
-          // spreadAllList();
         }
       }
     } catch (e) {
-      console.log('not found', e);
-      // if (isFirstTry) {
-      //   return await spreadAllList(false);
-      // } else {
-        resolve(true);
-      // }
+      resolve(true);
     }
   });
 }
 const url = urls.shift();
 startCrawling(url);
-
-
-// const a = overviewLinks.slice(0, 10);
-  // const b = overviewLinks.slice(10, 20);
-
-  // const secondDriver = new webdriver.Builder()
-  // .withCapabilities(chromeCapabilities)
-  // .build();
